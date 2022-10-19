51156cbf482b9ae77407f41ea03dd605acfd5fb5
Andrei Chirita <adchir95@gmail.com>
2022-10-19 20:55:27 +0000

    updating the SQL to have protection against injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,15 +8,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const userInput = process.argv.slice(2);
+const userCohort = process.argv.slice(2)[0];
+const userLimit = process.argv.slice(2)[1];
+const userInput = [`%${userCohort}%`, userLimit];
 
 pool.query(`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${userInput[0]}%'
-LIMIT ${userInput[1]};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, userInput)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,7 +8,7 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const userInput = process.argv.slice(2);
+const userInput = [`${process.argv.slice(2)}`];
 
 pool.query(`
 SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
@@ -16,10 +16,10 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${userInput[0]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`)
+`, userInput)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${userInput[0]}: ${user.teacher}`);
